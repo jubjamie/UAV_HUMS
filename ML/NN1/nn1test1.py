10b56982da950d56fddfe92a4b40292229bacd44
Jamie Cooke <jamie@iamjamiecooke.com>
2019-02-21 19:24:42 +0000

    Import some data and preprocess/split
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- ML/NN1/nn1test1.py
+++ ML/NN1/nn1test1.py
@@ -1,9 +1,40 @@
 import tensorflow as tf
 import pandas as pd
 import os
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
-for file in os.listdir("../../databin/test2/"):
+def get_motor_status(fdata):
+    motor_modes = fdata[['m1_mode', 'm2_mode', 'm3_mode', 'm4_mode']].iloc[0]
+    motor_extract = [motor_modes['m1_mode'], motor_modes['m2_mode'], motor_modes['m3_mode'],
+                     motor_modes['m4_mode']]
+    if all(i == 'healthy' for i in motor_extract):
+        motor_status = 0
+    else:
+        motor_status = 1
+    return motor_status
+
+
+data = []
+labels = np.array([])
+
+mydir = "../../databin/test2/"
+
+for file in os.listdir(mydir):
     if file.endswith(".csv"):
-        print(os.path.join("/databin/test2", file))
-        # flight_df = pd.read_csv(pickFile(), header=0, index_col=None)
+        filepath = os.path.join(mydir, file)
+        flight_df = pd.read_csv(filepath, header=0, index_col=None)
+        labels = np.append(labels, get_motor_status(flight_df))
+        flight_df = flight_df[['theta_error', 'phi_error', 'gamma_dot_error', 'theta_error_dot', 'phi_error_dot',
+                               'gamma_dot_error_dot']]
+        flight_df_n = flight_df.to_numpy()
+        data.append([flight_df_n])
+
+data = np.asarray(data)
+print(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25)
+
+print(X_train)
+print(y_train)
