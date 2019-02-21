ea74daa1cd679ca69228b8b3133012e148f31e55
Jamie Cooke <jamie@iamjamiecooke.com>
2019-02-21 18:16:23 +0000

    Small p2p changes
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- p2p_host.py
+++ p2p_host.py
@@ -2,13 +2,14 @@ import sim
 import simutilities
 
 # Set goals/yaws to go to
-GOALS = simutilities.randomgoals(3)
+GOALS = simutilities.randomgoals(1)
+# GOALS = [(0, 0, 3), (2, -2, 3), (-1.5, 1.5, 3)]
 
 newSim = sim.Sim()
 newSim.set_params(goals=GOALS, goal_time=15)
 newSim.set_failure_mode(setting='defined')
-newSim.see_gui = True
-newSim.see_motor_gui = True
-newSim.time_scale = 0.8
+newSim.see_gui = False
+newSim.see_motor_gui = False
+newSim.time_scale = 0
 newSim.ask_save_destination()
 newSim.run_sim()
