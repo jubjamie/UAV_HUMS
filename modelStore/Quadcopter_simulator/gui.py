ba22299b105c808aae5e3fa1daecf02577c2b80e
Jamie Cooke <jamie@iamjamiecooke.com>
2019-02-21 15:43:13 +0000

    Change gui to size 3D axes on goals
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- modelStore/Quadcopter_simulator/gui.py
+++ modelStore/Quadcopter_simulator/gui.py
@@ -5,18 +5,18 @@ import mpl_toolkits.mplot3d.axes3d as Axes3D
 import sys
 
 
-class GUI():
-    # 'quad_list' is a dictionary of format: quad_list = {'quad_1_name':{'position':quad_1_position,'orientation':quad_1_orientation,'arm_span':quad_1_arm_span}, ...}
+class GUI:
     def __init__(self, gui_mode, quads, goals, motor_modes):
         self.gui_mode = gui_mode
         self.quads = quads
         self.fig = plt.figure()
         self.ax = Axes3D.Axes3D(self.fig)
-        self.ax.set_xlim3d([-2.0, 2.0])
+        self.goals = np.asarray(goals)
+        self.ax.set_xlim3d([np.min(self.goals[:, 0]-2), np.max(self.goals[:, 0]+2)])
         self.ax.set_xlabel('X')
-        self.ax.set_ylim3d([-2.0, 2.0])
+        self.ax.set_ylim3d([np.min(self.goals[:, 1]-2), np.max(self.goals[:, ]+2)])
         self.ax.set_ylabel('Y')
-        self.ax.set_zlim3d([0, 5.0])
+        self.ax.set_zlim3d([0, np.max(self.goals[:, 2]+2)])
         self.ax.set_zlabel('Z')
         self.ax.set_title('Quadcopter Simulation')
         self.init_plot()
@@ -31,9 +31,9 @@ class GUI():
         if self.gui_mode is 2:
             self.thrust_fig, self.thrust_axs = plt.subplots(ncols=1, nrows=2)
             self.thrust_ax = self.thrust_axs[0]
-            self.thrust_mids = [1,2,3,4]
+            self.thrust_mids = [1, 2, 3, 4]
             self.thrust_values = [0, 0, 0, 0]
-            self.thrust_ax.set_ylim(0,13)
+            self.thrust_ax.set_ylim(0, 13)
             self.thrust_ypos = np.arange(len(self.thrust_mids))
             self.thrust_ax.set_xlabel('Thrust')
             self.thrust_ax.bar(self.thrust_mids, self.thrust_values)
@@ -44,24 +44,25 @@ class GUI():
             self.thrust_rpm_ax.set_xlabel('Requested RPMs')
             self.thrust_ax.bar(self.thrust_mids, self.rpm_values)
 
-    def rotation_matrix(self,angles):
+    def rotation_matrix(self, angles):
         ct = math.cos(angles[0])
         cp = math.cos(angles[1])
         cg = math.cos(angles[2])
         st = math.sin(angles[0])
         sp = math.sin(angles[1])
         sg = math.sin(angles[2])
-        R_x = np.array([[1,0,0],[0,ct,-st],[0,st,ct]])
-        R_y = np.array([[cp,0,sp],[0,1,0],[-sp,0,cp]])
-        R_z = np.array([[cg,-sg,0],[sg,cg,0],[0,0,1]])
-        R = np.dot(R_z, np.dot( R_y, R_x ))
+        R_x = np.array([[1, 0, 0], [0, ct, -st], [0, st, ct]])
+        R_y = np.array([[cp, 0, sp], [0, 1, 0], [-sp, 0, cp]])
+        R_z = np.array([[cg, -sg, 0], [sg, cg, 0], [0, 0, 1]])
+        R = np.dot(R_z, np.dot(R_y, R_x))
         return R
 
     def init_plot(self):
         for key in self.quads:
-            self.quads[key]['l1'], = self.ax.plot([],[],[],color='blue',linewidth=3,antialiased=False)
-            self.quads[key]['l2'], = self.ax.plot([],[],[],color='red',linewidth=3,antialiased=False)
-            self.quads[key]['hub'], = self.ax.plot([],[],[],marker='o',color='green', markersize=6,antialiased=False)
+            self.quads[key]['l1'], = self.ax.plot([], [], [], color='blue', linewidth=2, antialiased=False)
+            self.quads[key]['l2'], = self.ax.plot([], [], [], color='red', linewidth=2, antialiased=False)
+            self.quads[key]['hub'], = self.ax.plot([], [], [], marker='o', color='green', markersize=6,
+                                                   antialiased=False)
 
     def thrust_plot(self, thrust_values_in, rpm_values_in):
         self.thrust_ax.clear()
@@ -83,17 +84,17 @@ class GUI():
         for key in self.quads:
             R = self.rotation_matrix(self.quads[key]['orientation'])
             L = self.quads[key]['L']
-            points = np.array([ [-L,0,0], [L,0,0], [0,-L,0], [0,L,0], [0,0,0], [0,0,0] ]).T
-            points = np.dot(R,points)
-            points[0,:] += self.quads[key]['position'][0]
-            points[1,:] += self.quads[key]['position'][1]
-            points[2,:] += self.quads[key]['position'][2]
-            self.quads[key]['l1'].set_data(points[0,0:2],points[1,0:2])
-            self.quads[key]['l1'].set_3d_properties(points[2,0:2])
-            self.quads[key]['l2'].set_data(points[0,2:4],points[1,2:4])
-            self.quads[key]['l2'].set_3d_properties(points[2,2:4])
-            self.quads[key]['hub'].set_data(points[0,5],points[1,5])
-            self.quads[key]['hub'].set_3d_properties(points[2,5])
+            points = np.array([[-L, 0, 0], [L, 0, 0], [0, -L, 0], [0, L, 0], [0, 0, 0], [0, 0, 0]]).T
+            points = np.dot(R, points)
+            points[0, :] += self.quads[key]['position'][0]
+            points[1, :] += self.quads[key]['position'][1]
+            points[2, :] += self.quads[key]['position'][2]
+            self.quads[key]['l1'].set_data(points[0, 0:2], points[1, 0:2])
+            self.quads[key]['l1'].set_3d_properties(points[2, 0:2])
+            self.quads[key]['l2'].set_data(points[0, 2:4], points[1, 2:4])
+            self.quads[key]['l2'].set_3d_properties(points[2, 2:4])
+            self.quads[key]['hub'].set_data(points[0, 5], points[1, 5])
+            self.quads[key]['hub'].set_3d_properties(points[2, 5])
 
             if self.gui_mode is 2:
                 # Update thrust graph
@@ -112,7 +113,7 @@ class GUI():
     def stop_gui(self):
         plt.close('all')
 
-    def keypress_routine(self,event):
+    def keypress_routine(self, event):
         sys.stdout.flush()
         if event.key == 'x':
             y = list(self.ax.get_ylim3d())
