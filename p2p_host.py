5712cd56726af41ca2a798f76d062d8335a3e63d
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-07 18:46:18 +0000

    Rollback xy gains in testing seqs
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- p2p_host.py
+++ p2p_host.py
@@ -2,14 +2,14 @@ import sim
 import simutilities
 
 # Set goals/yaws to go to
-GOALS = simutilities.randomgoals(4)
-#GOALS = [(5, -5, 3), (-4, 4, 6)]
+# GOALS = simutilities.randomgoals(4)
+GOALS = [(5, -5, 3), (-4, 4, 6)]
 
 newSim = sim.Sim()
 newSim.set_params(goals=GOALS, goal_time=8)
-newSim.set_failure_mode(setting='defined')
-newSim.see_gui = True
+newSim.set_failure_mode(setting='random')
+newSim.see_gui = False
 newSim.see_motor_gui = False
-newSim.time_scale = 1
+newSim.time_scale = 0
 newSim.ask_save_destination()
 newSim.run_sim()
