4e940bd297a67d15b494e6785689353c14076df7
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-07 15:43:58 +0000

    Good attempt at tuning z linear
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- p2p_host.py
+++ p2p_host.py
@@ -8,8 +8,8 @@ GOALS = [(0, 0, 3), (2, -2, 3)]
 newSim = sim.Sim()
 newSim.set_params(goals=GOALS, goal_time=15)
 newSim.set_failure_mode(setting='defined')
-newSim.see_gui = False
-newSim.see_motor_gui = False
-newSim.time_scale = 0
+newSim.see_gui = True
+newSim.see_motor_gui = True
+newSim.time_scale = 1
 newSim.ask_save_destination()
 newSim.run_sim()
