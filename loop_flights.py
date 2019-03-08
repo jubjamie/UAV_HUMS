1291820cecbc522ae2c42d65ec2c47cf0db8931d
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-08 18:49:30 +0000

    Add line to loop to give simulation position indication
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- loop_flights.py
+++ loop_flights.py
@@ -8,7 +8,8 @@ GOALS = [(0, 0, 3), (2, -2, 3), (-1.5, 1.5, 3)]
 newSim = sim.Sim()
 newSim.set_params(goals=GOALS, goal_time=8)
 newSim.ask_save_destination()
-for i in range(500):
+for i in range(20):
+    print("Starting Simulation " + str(i+1))
     GOALS = simutilities.randomgoals(3)
     newSim.reset_goals_to(GOALS)
     newSim.set_failure_mode(setting='random')
