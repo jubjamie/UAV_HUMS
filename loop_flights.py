ce0cd415d1f5d0739b75ddc71df1544f632a0ba3
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-19 16:12:12 +0000

    Small loop flight conv update
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- loop_flights.py
+++ loop_flights.py
@@ -8,12 +8,12 @@ GOALS = [(0, 0, 3), (2, -2, 3), (-1.5, 1.5, 3)]
 newSim = sim.Sim()
 newSim.set_params(goals=GOALS, goal_time=8)
 newSim.ask_save_destination()
-for i in range(20):
+for i in range(100):
     print("Starting Simulation " + str(i+1))
     GOALS = simutilities.randomgoals(3)
     newSim.reset_goals_to(GOALS)
-    newSim.set_failure_mode(setting='random')
+    newSim.set_failure_mode(setting='defined', mode=['healthy', 'healthy', 'healthy', 'mf2'])
     newSim.see_gui = False
     newSim.time_scale = 0
     newSim.run_sim()
-    time.sleep(1)
+    time.sleep(0.3)
