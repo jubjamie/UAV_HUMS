20eda0d835a1aa4b72aeeb2d24a450c77009ca6a
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-19 16:45:31 +0000

    Add more complex looping logic to loop_flights
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- loop_flights.py
+++ loop_flights.py
@@ -6,14 +6,17 @@ import simutilities
 GOALS = [(0, 0, 3), (2, -2, 3), (-1.5, 1.5, 3)]
 
 newSim = sim.Sim()
-newSim.set_params(goals=GOALS, goal_time=8)
+newSim.set_params(goals=GOALS, goal_time=6)
 newSim.ask_save_destination()
-for i in range(100):
-    print("Starting Simulation " + str(i+1))
-    GOALS = simutilities.randomgoals(3)
-    newSim.reset_goals_to(GOALS)
-    newSim.set_failure_mode(setting='defined', mode=['healthy', 'healthy', 'healthy', 'mf2'])
-    newSim.see_gui = False
-    newSim.time_scale = 0
-    newSim.run_sim()
-    time.sleep(0.3)
+for k in range(0, 3):
+    h_string = ['healthy', 'healthy', 'healthy', 'healthy']
+    h_string[k] = 'rsf1c'
+    for i in range(2):
+        print("Starting Simulation " + str(i+1))
+        GOALS = simutilities.randomgoals(3)
+        newSim.reset_goals_to(GOALS)
+        newSim.set_failure_mode(setting='defined', mode=h_string)
+        newSim.see_gui = False
+        newSim.time_scale = 0
+        newSim.run_sim()
+        time.sleep(0.3)
