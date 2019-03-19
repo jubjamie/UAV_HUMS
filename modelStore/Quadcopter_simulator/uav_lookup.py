d22472dbc062c831cb557d2a4026d2fe66690158
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-19 16:30:24 +0000

    Fix major bug that used mean as std dev in noise production.
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- modelStore/Quadcopter_simulator/uav_lookup.py
+++ modelStore/Quadcopter_simulator/uav_lookup.py
@@ -16,7 +16,7 @@ def lookup_rpm(rpm, mid, mode='healthy'):
     """
     mode = parsemode(mode)
     newthrust_mean = np.interp(rpm, motordata.alldata[mode][:, 0], motordata.alldata[mode][:, 1])
-    newthrust_std = np.interp(rpm, motordata.alldata[mode][:, 0], motordata.alldata[mode][:, 1])
+    newthrust_std = np.interp(rpm, motordata.alldata[mode][:, 0], motordata.alldata[mode][:, 2])
     newthrust_g = np.clip(np.random.normal(newthrust_mean, newthrust_std), a_min=0, a_max=None)
     newthrust_n = newthrust_g * 9.81 / 1000
     return newthrust_n
