c688979e0e8ec75b9613b9c1f571c9e6689334bc
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-19 18:22:20 +0000

    Allow for multiple folders in datasource searching
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- ML/NN1/datasource.py
+++ ML/NN1/datasource.py
@@ -20,32 +20,34 @@ def get_motor_status(fdata):
 
 
 def get_data(asdict=False, newdata=False):
-    mydir = "../../databin/set3FriEve0308/"
+    mydir = ['../../databin/set1healthy/', '../../databin/set1mf2/', '../../databin/set1rsf1c/',
+             '../../databin/set1rsf4/']
     if newdata:
-        print('Fetching New Data...')
+        print('Fetching New Data...\nSearching Folders:')
         data = []
         labels = np.array([])
 
-
-        for file in os.listdir(mydir):
-            if file.endswith(".csv"):
-                filepath = os.path.join(mydir, file)
-                flight_df = pd.read_csv(filepath, header=0, index_col=None)
-                poll_label = get_motor_status(flight_df)
-                flight_df = flight_df[['theta_error', 'phi_error', 'theta_error_dot', 'phi_error_dot', 'theta_dot', 'phi_dot']]
-                flight_df_n = flight_df.to_numpy()
-                timepoints = flight_df_n.shape[0]
-                for j in range(pollingcycles):
-                    pollpoint = random.randint(0, timepoints-timepointwidth-1)
-                    flight_df_n_poll = flight_df_n[pollpoint:pollpoint+timepointwidth, :].T
-                    data.append(flight_df_n_poll)
-                    labels = np.append(labels, poll_label)
+        for dirx in mydir:
+            print(dirx)
+            for file in os.listdir(dirx):
+                if file.endswith(".csv"):
+                    filepath = os.path.join(dirx, file)
+                    flight_df = pd.read_csv(filepath, header=0, index_col=None)
+                    poll_label = get_motor_status(flight_df)
+                    flight_df = flight_df[['theta_error', 'phi_error', 'theta_error_dot', 'phi_error_dot', 'theta_dot', 'phi_dot']]
+                    flight_df_n = flight_df.to_numpy()
+                    timepoints = flight_df_n.shape[0]
+                    for j in range(pollingcycles):
+                        pollpoint = random.randint(0, timepoints-timepointwidth-1)
+                        flight_df_n_poll = flight_df_n[pollpoint:pollpoint+timepointwidth, :].T
+                        data.append(flight_df_n_poll)
+                        labels = np.append(labels, poll_label)
 
         data = np.asarray(data)
         labels = labels.astype(int)
         # Save new data
-        np.save(mydir + 'data.npy', data)
-        np.save(mydir + 'labels.npy', labels)
+        np.save(mydir[0] + 'data.npy', data)
+        np.save(mydir[0] + 'labels.npy', labels)
     else:
         print('Loading data from file...')
         data = np.load(mydir + 'data.npy')
@@ -53,6 +55,7 @@ def get_data(asdict=False, newdata=False):
 
     print(labels)
     print(type(labels))
+    print(data.shape)
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25)
     if asdict is True:
         X_train = {'theta_error': X_train[:, 0, :],
@@ -96,3 +99,6 @@ def eval_input_fn(features, labels, batch_size):
 
     # Return the dataset.
     return dataset
+
+
+get_data(newdata=True)
