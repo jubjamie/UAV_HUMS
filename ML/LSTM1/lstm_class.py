9d9208f1c9f91bb52f5f8535bca0bd58639c35ac
Jamie Cooke <jamie@iamjamiecooke.com>
2019-03-25 16:58:09 +0000

    Add in accuracy plotting and validation data
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- ML/LSTM1/lstm_class.py
+++ ML/LSTM1/lstm_class.py
@@ -17,16 +17,30 @@ print(y_test.shape)
 model = keras.Sequential([
     keras.layers.LSTM(input_shape=(X_train.shape[1], X_train.shape[2]), units=X_train.shape[1], return_sequences=True),
     keras.layers.Dropout(rate=0.2),
-    keras.layers.LSTM(8),
+    keras.layers.LSTM(16, return_sequences=True),
+    keras.layers.Dropout(rate=0.2),
+    keras.layers.LSTM(16),
     keras.layers.Dense(classes, activation='softmax')
 ])
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-model.fit(X_train, y_train, batch_size=128, epochs=16, shuffle=True)
+history = model.fit(X_train, y_train, batch_size=16, epochs=16, shuffle=True, validation_split=0.15)
 model.summary()
-model.save('models/lstm_class1.h5')
-test_loss, test_acc = model.evaluate(X_test, y_test, batch_size=128)
+model.save('models/lstm_class_' + str(X_train.shape[1]) + '_3x16.h5')
+
+# summarize history for accuracy
+plt.figure()
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='lower left')
+
+
+# Run tests
+test_loss, test_acc = model.evaluate(X_test, y_test, batch_size=16)
 
 print('Test Loss:', test_loss)
 print('Test Accuracy:', test_acc)
