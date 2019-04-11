564e87917be23133dd7066dfa418143a1bef6153
Jamie Cooke <jamie@iamjamiecooke.com>
2019-04-11 22:30:47 +0100

    Change p2p for bin testing.
    Signed-off-by: Jamie Cooke <jamie@iamjamiecooke.com>

--- p2p_host.py
+++ p2p_host.py
@@ -2,12 +2,12 @@ import sim
 import simutilities
 
 # Set goals/yaws to go to
-GOALS = simutilities.randomgoals(3)
-# GOALS = [(5, -5, 3), (-4, 4, 6)]
+# GOALS = simutilities.randomgoals(3)
+GOALS = [(5, -5, 3), (-4, 4, 6)]
 
 newSim = sim.Sim()
 newSim.set_params(goals=GOALS, goal_time=15)
-newSim.set_failure_mode(setting='random')
+newSim.set_failure_mode(setting='defined', mode=['mf2', 'healthy', 'healthy', 'healthy'])
 newSim.see_gui = False
 newSim.see_motor_gui = False
 newSim.time_scale = 0.5
